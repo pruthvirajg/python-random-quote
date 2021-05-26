ee0dc50b622e769aa785af9e956237a34816f8d5
Pruthvi G <pruthvi.gampalwar@gmail.com>
2021-05-27 00:42:35 +0530

    Printing the Random line of the Quotes.txt file

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,15 @@
 def primary():
     # print("Keep it logically awesome.")
+    
+  last = 13
+  rnd = random.randint(0, last)
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes[0])
+  print(quotes[rnd])
 
 if __name__== "__main__":
+  import random
   primary()
